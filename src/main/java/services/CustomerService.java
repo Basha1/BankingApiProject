6aa665285e9ebc957e193ccf69339a4ce3f70917
x15368301 <x15368301@SCR3-053.student.nci.col>
2018-12-03 17:41:12 +0000

    made changes to CustomerService

--- src/main/java/services/CustomerService.java
+++ src/main/java/services/CustomerService.java
@@ -10,7 +10,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/users")
+@Path("/customers")
 public class CustomerService {
     
     EntityManager entityManager;
